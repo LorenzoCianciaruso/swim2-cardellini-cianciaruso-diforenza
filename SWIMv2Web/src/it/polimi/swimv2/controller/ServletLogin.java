e60029f7aa53da252da688f167c1b163f7ab2307
albe.carde <albe.carde@gmail.com>
2012-12-20 13:34:23 +0000

    la

--- SWIMv2Web/src/it/polimi/swimv2/controller/ServletLogin.java
+++ SWIMv2Web/src/it/polimi/swimv2/controller/ServletLogin.java
@@ -2,8 +2,13 @@ package it.polimi.swimv2.controller;
 
 
 
+import it.polimi.swimv2.business.IUser;
+import it.polimi.swimv2.entities.User;
+
 import java.io.IOException;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -15,22 +20,54 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private static final String LOOKUP_STRING = "UserBean/remote";
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		IUser bean = doLookup();
 		
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-	
-		request.setAttribute("email", email);
-		request.setAttribute("password", password);
-			
-		forward(request, response, "/profile.jsp");
+		
+		User u = new User();
+		u.setEmail(email);
+		u.setPassword(password);
+		
+		User u2 = bean.findUserByLogin(u);
+		
+		if(u2.equals(null)){
+			response.encodeRedirectURL("login.jsp");
+		}else{
+			request.setAttribute("email", email);
+			request.setAttribute("password", password);
+			forward(request, response, "/profile.jsp");
+		}
+		
+		
+		
 		
 		
 	}
 	
+	private IUser doLookup() {
+		Context context = null;
+		IUser bean = null;
+		
+		try {
+            // 1. Obtaining Context
+            context = it.polimi.swimv2.clientutility.JNDILookupClass.getInitialContext();
+            // 2. Lookup and cast
+            bean = (IUser) context.lookup(LOOKUP_STRING);
+ 
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+		
+		return bean;
+	}
+	
+	
+
 	private void forward(HttpServletRequest request, HttpServletResponse response, String page) throws ServletException, IOException
 	{
 		ServletContext sc = getServletContext(); 
