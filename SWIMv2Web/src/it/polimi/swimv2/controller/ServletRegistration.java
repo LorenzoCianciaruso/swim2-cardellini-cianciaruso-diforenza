4c3866f5694cd16a7db40b0fd298c3d2219ed820
francesco.diforenza <francesco.diforenza@gmail.com>
2012-12-22 13:49:03 +0000

    commenti e aggiunto il TODO del rinominare la pagina

--- SWIMv2Web/src/it/polimi/swimv2/controller/ServletRegistration.java
+++ SWIMv2Web/src/it/polimi/swimv2/controller/ServletRegistration.java
@@ -13,10 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+// manage registration to the site
 public class ServletRegistration extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String LOOKUP_STRING = "UserBean/remote";
 
+	//get info and create new entity
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		IUser bean = doLookup();
