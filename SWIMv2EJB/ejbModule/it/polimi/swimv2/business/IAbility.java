b963cf001f40f1b1974bf5ba1b1e541016a61ae5
albe.carde <albe.carde@gmail.com>
2013-01-03 01:57:42 +0000

    dio

---
+++ SWIMv2EJB/ejbModule/it/polimi/swimv2/business/IAbility.java
@@ -0,0 +1,14 @@
+package it.polimi.swimv2.business;
+
+import it.polimi.swimv2.entities.Ability;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface IAbility {
+
+	List<Ability> findAllAbilities();
+
+}
